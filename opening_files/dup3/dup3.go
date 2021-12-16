3c5d1383bc20f1200792f781f912b6b04ecbd48f
Giorgos Alexakis <58263228+Looper2074@users.noreply.github.com>
2021-12-16 20:21:37 +0200

    Added dup3.

---
+++ opening_files/dup3/dup3.go
@@ -0,0 +1,29 @@
+// dup3 only reads from files but instead of processing one line at a time, it loads the entire text in memory
+// by using ioutil.Readfile() and then splits the data read ( converts the byte array into a string first ) by line .
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
