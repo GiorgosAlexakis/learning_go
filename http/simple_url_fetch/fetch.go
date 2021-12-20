99c745b035b246daf776e7828d96e98c3955a039
Giorgos Alexakis <58263228+Looper2074@users.noreply.github.com>
2021-12-20 10:44:57 +0200

    Format the error of the url fetch

--- http/simple_url_fetch/fetch.go
+++ http/simple_url_fetch/fetch.go
@@ -12,7 +12,7 @@ func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
-			fmt.Fprint(os.Stderr, "fetch: %\n", err)
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 		b, err := ioutil.ReadAll(resp.Body)
