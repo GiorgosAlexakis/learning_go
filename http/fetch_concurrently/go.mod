5a160bdbe8fd11f83119a9b2e918e16b49c70530
Giorgos Alexakis <58263228+Looper2074@users.noreply.github.com>
2021-12-20 18:55:49 +0200

    added fetch concurrently

---
+++ http/fetch_concurrently/go.mod
@@ -0,0 +1,3 @@
+module fetch_concurrently
+
+go 1.17
--- http/simple_url_fetch/fetch.go
+++ http/simple_url_fetch/fetch.go
@@ -21,6 +21,8 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Printf("----------- HTTP Status Code: %s -----------\n", resp.Status)
+		fmt.Println()
 		fmt.Printf("%s", b)
 	}
 }
