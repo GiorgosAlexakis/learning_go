0a6358a3846c9fa9a87833eb448fc31876ac0c06
Giorgos Alexakis <58263228+Looper2074@users.noreply.github.com>
2021-12-26 11:09:00 +0200

    Added tempconv0

---
+++ types/tempconv0/tempconv0.go
@@ -0,0 +1,38 @@
+// Package tempconv performs Celsius and Fahrenheit temperature computations.
+package main
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+/*
+	The declaration below, in which the Celsius parameter c appears
+	before the function name, associates with the Celsius type a method
+	named String that returns c’s numeric value followed by °C
+*/
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+
+func main() {
+	fmt.Printf("%g\n", BoilingC-FreezingC)
+	boilingF := CToF(BoilingC)
+	fmt.Printf("%g\n", boilingF-CToF(FreezingC))
+	// fmt.Printf("%g\n", boilingF-FreezingC) mismatched types Fahrenheit and Celsius
+	c := FToC(212.0)
+	fmt.Println(c.String())
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%s\n", c)
+	fmt.Println(c)
+	fmt.Printf("%g\n", c)
+	fmt.Println(float64(c))
+}
