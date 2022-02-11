dc474b0f11f1f4c0098aef642c2e7ca8198e0cd3
Looper2074 <58263228+Looper2074@users.noreply.github.com>
2022-02-11 19:24:30 +0200

    ssh_client_config

---
+++ ssh/ssh_client_config_example/go.mod
@@ -0,0 +1,7 @@
+module ssh_client_config_example
+
+go 1.17
+
+require golang.org/x/crypto v0.0.0-20220128200615-198e4374d7ed
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
---
+++ ssh/ssh_client_config_example/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	host := "127.0.0.1:22"
+	user := "george"
+	var err error
+	var signer ssh.Signer
+	b, err := ioutil.ReadFile("/home/george/.ssh/id_rsa")
+	signer, err = ssh.ParsePrivateKey(b)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	conf := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	var conn *ssh.Client
+
+	conn, err = ssh.Dial("tcp", host, conf)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer conn.Close()
+
+	var session *ssh.Session
+	var stdin io.WriteCloser
+	var stdout, stderr io.Reader
+
+	session, err = conn.NewSession()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer session.Close()
+
+	stdin, err = session.StdinPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stdout, err = session.StdoutPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stderr, err = session.StderrPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	wr := make(chan []byte, 10)
+
+	go func() {
+		err = session.Run("go")
+		for {
+			select {
+			case d := <-wr:
+				_, err := stdin.Write(d)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for {
+			if tkn := scanner.Scan(); tkn {
+				rcv := scanner.Bytes()
+
+				raw := make([]byte, len(rcv))
+				copy(raw, rcv)
+
+				fmt.Println(string(raw))
+			} else {
+				if scanner.Err() != nil {
+					fmt.Println(scanner.Err())
+				} else {
+					fmt.Println("io.EOF")
+				}
+				return
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	session.Shell()
+
+	for {
+		fmt.Println("$")
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+
+		wr <- []byte(text + "\n")
+	}
+}
