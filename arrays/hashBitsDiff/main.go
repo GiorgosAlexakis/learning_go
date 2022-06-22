53c660c05f6dc1ad0c4100fac015d851e4e73dac
Giorgos Alexakis <galexakhs97@gmail.com>
2022-06-22 14:41:09 +0300

    fix hashBitsDiff

--- arrays/hashBitsDiff/main.go
+++ arrays/hashBitsDiff/main.go
@@ -5,21 +5,17 @@ import (
 	"log"
 )
 
-var pc [256]byte
-
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
 func countDiff(a *[32]byte, b *[32]byte) (count int) {
 	for i := 0; i < len(*a); i++ {
 		for j := 0; j < 8; j++ {
-			if int(pc[a[i]>>(j*8)]) != int(pc[b[i]>>(j*8)]) {
+			if (int(a[i]>>(j)) & 1) != (int(b[i]>>(j)) & 1) {
 				count++
 			}
 		}
+		log.Println(count)
+		log.Printf("a[i]: %08b\n", a[i])
+		log.Printf("b[i]: %08b\n\n", b[i])
+
 	}
 	return count
 }
